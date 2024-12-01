549529e2ef78db762070bf7ba22c55e9c7aa62d8
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2024-12-01 23:51:22 +0530

    Merge remote-tracking branch 'origin/main'

--- module-ec2/main.tf
+++ module-ec2/main.tf
@@ -51,7 +51,6 @@ resource "aws_route53_record" "record-public" {
 }
 
 
-
   resource "aws_route53_record" "record-internal" {
   zone_id = var.zone_id
   name    = "${var.tool_name}-internal.${var.domain_name}"
