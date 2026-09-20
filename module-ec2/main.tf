9ac81042723f825ebe6a10c65557c58dae5a9ddd
Abdulqadirsidd <abdulqadirsidd2011@gmail.com>
2026-09-20 17:38:29 +0530

    project roboshop 26

--- module-ec2/main.tf
+++ module-ec2/main.tf
@@ -35,10 +35,10 @@ resource "aws_instance" "instance" {
   tags = {
     Name = var.tool_name
   }
-  root_block_device {
-    volume_size = var.volume_size
-  }
-}
+#   root_block_device {
+#     volume_size = var.volume_size
+#   }
+# }
 
   instance_market_options {
     market_type = "spot"
